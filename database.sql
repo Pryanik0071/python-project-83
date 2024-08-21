218c27efff9c9e5ee59a6de380a1003f7e09152f
Dmitry <di.mesler.2010@yandex.ru>
2024-08-22 01:17:38 +0300

    Update: database.sql, add url_checks

--- database.sql
+++ database.sql
@@ -2,6 +2,20 @@
 
 CREATE TABLE public.urls (
 	id bigserial NOT NULL,
-	"name" varchar NULL,
-	created_at date NULL
+	name varchar NULL,
+	created_at date NULL,
+	CONSTRAINT urls_pk PRIMARY KEY (id)
+);
+
+-- DROP TABLE public.url_checks;
+
+CREATE TABLE public.url_checks (
+	id bigserial NOT NULL,
+	url_id int8 NOT NULL,
+	status_code int4 NULL,
+	h1 varchar NULL,
+	title varchar NULL,
+	description varchar NULL,
+	created_at date NULL,
+	CONSTRAINT url_checks_fk FOREIGN KEY (url_id) REFERENCES public.urls(id)
 );
