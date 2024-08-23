55462b08a1920d6fd77842912973923f5c5483fd
Dmitry Mesler <41776673+Pryanik0071@users.noreply.github.com>
2024-08-23 13:34:22 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,14 @@
--- Schema: public
+DROP TABLE IF EXISTS url_checks;
+DROP TABLE IF EXISTS urls;
 
--- DROP TABLE  IF EXISTS public.url_checks;
+CREATE TABLE urls (
+	id bigserial NOT NULL,
+	name varchar NULL,
+	created_at date NULL,
+	CONSTRAINT urls_pk PRIMARY KEY (id)
+);
 
-CREATE TABLE public.url_checks (
+CREATE TABLE url_checks (
 	id bigserial NOT NULL,
 	url_id int8 NOT NULL,
 	status_code int4 NULL,
@@ -10,14 +16,5 @@ CREATE TABLE public.url_checks (
 	title varchar NULL,
 	description varchar NULL,
 	created_at date NULL,
-	CONSTRAINT url_checks_fk FOREIGN KEY (url_id) REFERENCES public.urls(id)
-);
-
--- DROP TABLE  IF EXISTS public.urls;
-
-CREATE TABLE public.urls (
-	id bigserial NOT NULL,
-	name varchar NULL,
-	created_at date NULL,
-	CONSTRAINT urls_pk PRIMARY KEY (id)
+	CONSTRAINT url_checks_fk FOREIGN KEY (url_id) REFERENCES urls(id)
 );
